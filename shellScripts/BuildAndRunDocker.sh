94c008d8b359b5857424c22c14d8d0b2550a28f9
YYTWINT <yytwint.sisw@siemens.com>
2023-05-31 18:20:09 +0530

    Update BuildAndRunDocker.sh

--- shellScripts/BuildAndRunDocker.sh
+++ shellScripts/BuildAndRunDocker.sh
@@ -13,7 +13,7 @@ STAGE_DIR=$2/TranslatorBinaries
 
 docker build -t trx22:stepjt $STAGE_DIR -f $STAGE_DIR/dockerfile || { exit 1;}
 
-docker run --name stepjt_testrun_container -v /apps/JenkinsBase/docker/step:/volume --cpus="1" --memory="2g" -itd trx22:stepjt
+docker run --name stepjt_testrun_container -v /apps/JenkinsBase/docker:/volume --cpus="1" --memory="2g" -itd trx22:stepjt
 
 #Now check for error in /volume/Logs/log.txt file
 LOG_FILE=/apps/JenkinsBase/docker/step/Logs/log_pass.txt

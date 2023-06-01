ce3e3bdaa7afe847bb2d065f0c8c4a76d3f9666e
YYTWINT <yytwint.sisw@siemens.com>
2023-06-01 21:19:11 +0530

    Update BuildAndRunDocker.sh

--- shellScripts/BuildAndRunDocker.sh
+++ shellScripts/BuildAndRunDocker.sh
@@ -27,7 +27,7 @@ echo "Checking case for pass condition"
 if [ -f $LOG_FILE ] 
 then
 	echo $LOG_FILE
-	for failingCase in `grep ":137" $LOG_FILE`
+	for failingCase in [`grep ":137" $LOG_FILE`]
 	do 
 		echo "Welcome $c times"
 	done

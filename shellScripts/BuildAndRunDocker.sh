9da0885d91bf2b0c773b78449d2ed27e06141771
YYTWINT <yytwint.sisw@siemens.com>
2023-06-02 07:48:32 +0530

    Update BuildAndRunDocker.sh

--- shellScripts/BuildAndRunDocker.sh
+++ shellScripts/BuildAndRunDocker.sh
@@ -27,7 +27,7 @@ echo "Checking case for pass condition"
 if [ -f $LOG_FILE ] 
 then
 	echo $LOG_FILE
-	for containerId in $(cat /apps/JenkinsBase/docker/step/Logs/testing.txt)
+	for containerId in $(cat /apps/JenkinsBase/docker/step/Logs/testing.txt | grep "1")
 	do
 		echo "container with ID $containerId"
 	done
